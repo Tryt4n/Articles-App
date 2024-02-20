0394147e0260dc88fa4225d0c2a6f2f85a394867
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-20 15:31:18 +0100

    Users and posts functions have been separated

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,40 +1,20 @@
-import { unstable_cache } from "next/cache";
-import { cache } from "react";
 import prisma from "./db";
+import { cache } from "react";
+import { unstable_cache } from "next/cache";
 import { wait } from "@/app/_helpers/helpers";
 
-// export const fetchAllUsers = unstable_cache(
-//   cache(async () => {
-//     await wait(1000);
-//     return prisma.user.findMany();
-//   }),
-//   ["users"]
-// );
+export const fetchAllUsers = unstable_cache(
+  cache(async () => {
+    // await wait(1000);
+    return prisma.user.findMany();
+  }),
+  ["users"]
+);
 
-// export const fetchAllPosts = unstable_cache(
-//   cache(async () => {
-//     await wait(1000);
-//     return prisma.post.findMany();
-//   }),
-//   ["posts"]
-// );
-
-export async function fetchAllUsers() {
-  await wait(1000);
-  return prisma.user.findMany();
-}
-
-export async function fetchAllPosts() {
-  await wait(1000);
-  return prisma.post.findMany();
-}
-
-export async function fetchPost({ id }: { id: string }) {
-  await wait(1000);
-  return prisma.post.findUnique({ where: { id } });
-}
-
-export async function fetchUser({ id }: { id: string }) {
-  await wait(1000);
-  return prisma.user.findUnique({ where: { id } });
-}
+export const fetchUser = unstable_cache(
+  cache(async ({ id }: { id: string }) => {
+    // await wait(1000);
+    return prisma.user.findUnique({ where: { id } });
+  }),
+  ["user"]
+);
