9780e3ecc84f0d43d7c278d4139d235163108c64
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-21 11:27:28 +0100

    Wrapped by `<mark>` element searched phrase.

--- src/app/_components/Card.tsx
+++ src/app/_components/Card.tsx
@@ -4,11 +4,32 @@ import Image from "next/image";
 import { fetchUser } from "@/db/users";
 import { format } from "date-fns/format";
 import type { Post } from "@prisma/client";
+import type { SearchProps } from "../page";
 
-export default async function Card({ post, priority }: { post: Post; priority: boolean }) {
+export default async function Card({
+  post,
+  priority,
+  searchParams,
+}: {
+  post: Post;
+  priority: boolean;
+  searchParams: SearchProps["searchParams"];
+}) {
   const author = await fetchUser({ id: post.authorId });
 
   const postPublishedDate = new Date(post.publishedAt!);
+
+  function markSearchedPhrase(text: string) {
+    return text
+      .split(new RegExp(`(${searchParams.query})`, "gi"))
+      .map((part, index) =>
+        part.toLowerCase() === searchParams.query.toLowerCase() ? (
+          <mark key={index}>{part}</mark>
+        ) : (
+          part
+        )
+      );
+  }
 
   return (
     <li className="card-container">
@@ -26,32 +47,43 @@ export default async function Card({ post, priority }: { post: Post; priority: b
           </div>
 
           <div className="card-content-wrapper">
-            <h2 className="card-header">{post.title}</h2>
+            <h2 className="card-header">
+              {searchParams.filterBy === "title" && searchParams.query !== ""
+                ? markSearchedPhrase(post.title)
+                : post.title}
+            </h2>
             <p className="card-subheader">{post.firstWords}</p>
 
             <div className="card-content-inner-wrapper">
               <div className="card-details">
-                <Image
-                  src={author?.avatar || ""}
-                  alt={`${author?.name} avatar`}
-                  width={40}
-                  height={40}
-                  className="card-avatar-image card-image-placeholder "
-                />
-                <div className="card-details-inner">
-                  <span
-                    title="Author"
-                    itemProp="author"
-                  >
-                    {author?.name}
-                  </span>
-                  <time
-                    dateTime={postPublishedDate.toLocaleDateString()}
-                    className="card-details-time"
-                  >
-                    {format(postPublishedDate, "d MMM yyyy")}
-                  </time>
-                </div>
+                {author ? (
+                  <>
+                    <Image
+                      src={author.avatar}
+                      alt={`${author.name} avatar`}
+                      width={40}
+                      height={40}
+                      className="card-avatar-image card-image-placeholder "
+                    />
+                    <div className="card-details-inner">
+                      <span
+                        title="Author"
+                        itemProp="author"
+                      >
+                        {searchParams.filterBy === "author" && searchParams.query !== ""
+                          ? //TODO Change delete `as` casting after reloading database
+                            markSearchedPhrase(author.name as string)
+                          : author!.name}
+                      </span>
+                      <time
+                        dateTime={postPublishedDate.toLocaleDateString()}
+                        className="card-details-time"
+                      >
+                        {format(postPublishedDate, "d MMM yyyy")}
+                      </time>
+                    </div>
+                  </>
+                ) : null}
               </div>
 
               <p
