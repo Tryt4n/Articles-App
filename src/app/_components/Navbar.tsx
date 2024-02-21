4aad79d3023dd643a2a9c57d367defd0abdcf394
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-21 14:03:50 +0100

    Added showing/hiding some link/icons based on `next-auth` session.

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,8 +1,13 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
 import NavLink from "./NavLink";
+import Image from "next/image";
+import { getServerSession } from "next-auth/next";
+import { options } from "../api/auth/[...nextauth]/options";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await getServerSession(options);
+
   return (
     <nav className="navbar">
       <ul>
@@ -12,21 +17,35 @@ export default function Navbar() {
         <li>
           <NavLink href="/authors">Authors</NavLink>
         </li>
+        {session && (
+          <li>
+            <NavLink href="/drafts">Drafts</NavLink>
+          </li>
+        )}
       </ul>
 
       <div className="auth-container">
         <Link
-          href="/login"
+          href={session ? "/api/auth/signout" : "/api/auth/signin"}
           className="btn btn--accent"
         >
-          Login
+          {session ? "Logout" : "Login"}
         </Link>
-        <Link
-          href="/signup"
-          className="btn"
-        >
-          Signup
-        </Link>
+        {!session ? (
+          <Link
+            href="/signup"
+            className="btn"
+          >
+            Signup
+          </Link>
+        ) : (
+          <Image
+            src={session.user?.image || ""}
+            alt="user image"
+            width={50}
+            height={50}
+          />
+        )}
       </div>
     </nav>
   );
