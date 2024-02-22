4af567a68326aa4830ad665809f779d4ad7a5aab
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-22 10:03:34 +0100

    Deleted fetch for users and replaced it with fetch only authors.

--- src/db/users.ts
+++ src/db/users.ts
@@ -2,14 +2,7 @@ import prisma from "./db";
 import { cache } from "react";
 import { unstable_cache } from "next/cache";
 import { wait } from "@/app/_helpers/helpers";
-
-export const fetchAllUsers = unstable_cache(
-  cache(async () => {
-    // await wait(1000);
-    return prisma.user.findMany();
-  }),
-  ["users"]
-);
+import type { UserRole } from "@/types/users";
 
 export const fetchUser = unstable_cache(
   cache(async ({ id }: { id: string }) => {
@@ -17,4 +10,21 @@ export const fetchUser = unstable_cache(
     return prisma.user.findUnique({ where: { id } });
   }),
   ["user"]
+);
+
+export const fetchAllAuthors = unstable_cache(
+  // await wait(1000);
+  cache(async () => {
+    const authors = await prisma.user.findMany({
+      where: {
+        role: "moderator",
+      },
+    });
+
+    return authors.map((author) => ({
+      ...author,
+      role: author.role as UserRole,
+    }));
+  }),
+  ["authors"]
 );
