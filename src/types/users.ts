0870f2b7b553371ac340c36f8f79abeb86de5563
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-23 16:46:40 +0100

    Added type for the user.

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,1 +1,10 @@
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string | null;
+  image: string;
+  role: UserRole;
+};
+
 export type UserRole = "user" | "moderator" | "admin";
