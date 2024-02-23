c847f985cf23d1f2488661e8bf0133489818889f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-23 08:52:06 +0100

    Added redirecting from signup page to homepage if user is already logged in.

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React, { useEffect } from "react";
+import SignupForm from "./SignupForm";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export default function SignupPage() {
+  const { status } = useSession();
+  const isUserLoggedIn = status === "authenticated";
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isUserLoggedIn) {
+      router.push("/");
+    }
+  }, [isUserLoggedIn, router]);
+
+  return (
+    <>
+      {status === "loading" && (
+        <main className="container">
+          <h1>Loading...</h1>
+        </main>
+      )}
+      {status !== "loading" && status !== "authenticated" ? (
+        <main className="container">
+          <h1>Signup</h1>
+
+          <SignupForm />
+        </main>
+      ) : null}
+    </>
+  );
+}
