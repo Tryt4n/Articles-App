3d4019979b3dd22fa7178db65003486e8eeda66f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-23 16:06:03 +0100

    Adjusted the creation of a new user with `zod`.

--- src/app/_actions/users.ts
+++ src/app/_actions/users.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import { createNewUser } from "@/db/users";
-import { validateCreationOfNewUser } from "../helpers/validation";
+import { createNewUser, isNewUserEmailUnique, isNewUserUsernameUnique } from "@/db/users";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import { SignupSchema } from "@/zod/userSchema";
 
 export async function signupUserAction(prevState: unknown, formData: FormData) {
   const email = formData.get("signup-email") as string;
@@ -11,62 +11,44 @@ export async function signupUserAction(prevState: unknown, formData: FormData) {
   const password = formData.get("signup-password") as string;
   const passwordConfirmation = formData.get("signup-password-confirmation") as string;
 
-  //   const isEmailUnique = await isNewUserEmailUnique(email);
-  //   const isUsernameUnique = await isNewUserUsernameUnique(username);
+  let customErrors: string[] = [];
+  const isEmailUnique = await isNewUserEmailUnique(email);
+  const isUsernameUnique = await isNewUserUsernameUnique(username);
 
-  //   if (email === "") {
-  //     return { errorMessage: "Email is required" };
-  //   }
-  //   if (username === "") {
-  //     return { errorMessage: "Username is required" };
-  //   }
-  //   if (password === "") {
-  //     return { errorMessage: "Password is required" };
-  //   }
-  //   if (passwordConfirmation === "") {
-  //     return { errorMessage: "Password confirmation is required" };
-  //   }
-  //   if (!email.includes("@") || email.length < 5 || email.length > 50) {
-  //     return { errorMessage: "Email is invalid" };
-  //   }
-  //   if (!isEmailUnique) {
-  //     return { errorMessage: "Email is already taken" };
-  //   }
-  //   if (!isUsernameUnique) {
-  //     return { errorMessage: "Username is already taken" };
-  //   }
-  //   if (password.length < 8) {
-  //     return { errorMessage: "Password must be at least 8 characters" };
-  //   }
-  //   if (password.length > 50) {
-  //     return { errorMessage: "Password must be at most 50 characters" };
-  //   }
-  //   if (password !== passwordConfirmation) {
-  //     return { errorMessage: "Passwords do not match" };
-  //   }
+  if (!isEmailUnique) customErrors.push("Email is already taken.");
+  if (!isUsernameUnique) customErrors.push("Username is already taken.");
 
-  // const isValidationPassed = await validateCreationOfNewUser(email,username,password,passwordConfirmation);
-
-  //   await createNewUser(newUser);
-
-  const { isValid, error } = await validateCreationOfNewUser(
+  const validationData = {
     email,
     username,
     password,
-    passwordConfirmation
-  );
-
-  const newUser = {
-    name: username,
-    email,
-    password,
-    image: "https://source.unsplash.com/random/60x60",
+    passwordConfirmation,
   };
 
-  if (isValid) {
-    await createNewUser(newUser);
-    redirect("/api/auth/signin");
+  const results = SignupSchema.safeParse(validationData);
+
+  if (!isEmailUnique || !isUsernameUnique) {
+    if (!results.success) {
+      const errorMessages = [
+        ...customErrors,
+        ...results.error.issues.map((issue) => issue.message),
+      ];
+
+      return errorMessages;
+    }
   } else {
-    return { errorMessage: error };
+    const newUser = {
+      email,
+      name: username,
+      password,
+      image: "https://source.unsplash.com/random/60x60",
+    };
+
+    await createNewUser(newUser);
+
+    revalidatePath("/");
+    revalidatePath("/profile");
+    revalidatePath("/signup");
+    redirect("/api/auth/signin");
   }
 }
