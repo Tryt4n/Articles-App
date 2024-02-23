19868dd04657f3dad837771e2d9a290f8dc7186e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-23 16:09:24 +0100

    Added some temporary placeholder content.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { fetchUser } from "@/db/users";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/authOptions";
 
-export default function ProfilePage() {
-  return <h1>Profile</h1>;
+export default async function ProfilePage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user ? await fetchUser({ id: session.user.id }) : null;
+
+  return (
+    <main>
+      <h1>Profile</h1>
+      {user && (
+        <>
+          <div>Name: {user.name}</div>
+          <div>User ID: {user.id}</div>
+          <div>User Role: {user.role}</div>
+        </>
+      )}
+    </main>
+  );
 }
