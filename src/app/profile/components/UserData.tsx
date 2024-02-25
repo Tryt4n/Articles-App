cfe4997817ee83bd34f5b2884a84bd26240ae498
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-25 16:51:35 +0100

    Created component for profile page.

---
+++ src/app/profile/components/UserData.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { fetchUser } from "@/db/users";
+import Link from "next/link";
+import { format } from "date-fns/format";
+import type { User } from "@/types/users";
+
+export default function UserData({ user }: { user: User }) {
+  const formatDate = (date: Date) => format(date, "H:mm, dd.MM.yyyy");
+
+  return (
+    <>
+      {user && (
+        <>
+          <section>
+            <h2>Your profile date</h2>
+            <div>
+              <p>Name: {user.name}</p>
+              <button>Edit</button>
+            </div>
+
+            <div>
+              <p>Email: {user.email}</p>
+              <button>Edit</button>
+            </div>
+
+            {user.receivedLikes && user.receivedLikes.length > 0 && (
+              <div>
+                <p>Likes: {user.receivedLikes.length}</p>
+              </div>
+            )}
+          </section>
+
+          {user.comments && user.comments.length > 0 && (
+            <section>
+              <h2>Your activity</h2>
+              <ul>
+                {user.comments.map((comment) => {
+                  const formattedCommentDate = formatDate(comment.createdAt);
+                  const formattedUpdatedCommentDate = formatDate(comment.updatedAt);
+
+                  const isUpdated = formattedCommentDate !== formattedUpdatedCommentDate;
+                  const displayDate = isUpdated
+                    ? formattedUpdatedCommentDate
+                    : formattedCommentDate;
+                  const displayText = isUpdated ? "Updated at: " : "Created at: ";
+
+                  return (
+                    <li key={comment.id}>
+                      <p>{comment.content}</p>
+                      <time dateTime={displayDate}>
+                        {displayText} {displayDate}
+                      </time>
+                      <Link href={`/posts/${comment.postId}#${comment.id}`}>Click to see</Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </section>
+          )}
+
+          {user.followings && user.followings.length > 0 && (
+            <section>
+              <h2>My Followings</h2>
+              <ul>
+                {user.followings.map(async (follow) => {
+                  const followedUser = await fetchUser({ id: follow.followingUserId });
+                  const formattedDate = formatDate(follow.createdAt);
+
+                  return (
+                    <li key={follow.id}>
+                      <Link href={`/authors/${followedUser.id}`}>{followedUser.name}</Link>
+                      <time dateTime={formattedDate}>Followed at: {formattedDate}</time>
+                    </li>
+                  );
+                })}
+              </ul>
+            </section>
+          )}
+        </>
+      )}
+    </>
+  );
+}
