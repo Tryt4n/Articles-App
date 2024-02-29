74cfefef5a8d6fd740d4f57808ca712b3ed31830
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-29 09:06:52 +0100

    Added functionality of editing password.

--- src/app/profile/edit/(editSettings)/email/page.tsx
+++ src/app/profile/edit/(editSettings)/email/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { redirect } from "next/navigation";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { fetchUser } from "@/db/users";
 import { getServerSession } from "next-auth/next";
-import UserData from "../../components/UserData";
-import EditProfileForm from "../../components/EditProfileForm";
-import EditProfileBtn from "../../components/EditProfileBtn";
+import UserData from "../../../components/UserData";
+import EditProfileForm from "../../../components/EditProfileForm";
+import EditProfileBtn from "../../../components/EditProfileBtn";
 import type { Metadata } from "next/types";
 
 export const metadata: Metadata = {
@@ -14,6 +15,8 @@ export const metadata: Metadata = {
 export default async function ProfileEditEmailPage() {
   const session = await getServerSession(authOptions);
   const user = session?.user && (await fetchUser({ id: session.user.id }));
+
+  if (session?.user.name == null) redirect("/profile");
 
   return (
     <>
@@ -25,6 +28,8 @@ export default async function ProfileEditEmailPage() {
             userId={user.id}
             email={user.email}
           />
+
+          <EditProfileBtn password />
         </UserData>
       )}
     </>
--- src/app/profile/edit/(editSettings)/password/page.tsx
+++ src/app/profile/edit/(editSettings)/password/page.tsx
@@ -1,30 +1,35 @@
 import React from "react";
+import { redirect } from "next/navigation";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { fetchUser } from "@/db/users";
 import { getServerSession } from "next-auth/next";
-import UserData from "../../components/UserData";
-import EditProfileForm from "../../components/EditProfileForm";
-import EditProfileBtn from "../../components/EditProfileBtn";
+import UserData from "../../../components/UserData";
+import EditProfileForm from "../../../components/EditProfileForm";
+import EditProfileBtn from "../../../components/EditProfileBtn";
 import type { Metadata } from "next/types";
 
 export const metadata: Metadata = {
-  title: "Profile - edit name",
+  title: "Profile - edit password",
 };
 
 export default async function ProfileEditNamePage() {
   const session = await getServerSession(authOptions);
   const user = session?.user && (await fetchUser({ id: session.user.id }));
 
+  if (session?.user.name == null) redirect("/profile");
+
   return (
     <>
       {user && (
         <UserData user={user}>
-          <EditProfileForm
-            userId={user.id}
-            name={user.name}
-          />
+          <EditProfileBtn name={user.name} />
 
           <EditProfileBtn email={user.email} />
+
+          <EditProfileForm
+            userId={user.id}
+            password={true}
+          />
         </UserData>
       )}
     </>
