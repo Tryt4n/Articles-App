3d5766df56b71fb0afe570967ced9842a18ec07c
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-29 09:03:17 +0100

    Added functionality for `password`.

--- src/app/profile/components/EditProfileBtn.tsx
+++ src/app/profile/components/EditProfileBtn.tsx
@@ -5,18 +5,41 @@ type EditButton =
   | {
       email: string;
       name?: undefined;
+      password?: undefined;
     }
   | {
       email?: undefined;
       name: string;
+      password?: undefined;
+    }
+  | {
+      email?: undefined;
+      name?: undefined;
+      password: true | null;
     };
 
-export default function EditProfileBtn({ email, name }: EditButton) {
+export default function EditProfileBtn({ email, name, password }: EditButton) {
+  let type: "email" | "name" | "password";
+  let value: string | undefined;
+
+  if (email) {
+    type = "email";
+    value = email;
+  } else if (name) {
+    type = "name";
+    value = name;
+  } else if (password) {
+    type = "password";
+    value = "";
+  } else return null;
+
   return (
     <div className="profile-content-wrapper">
-      {email ? <p>Email: {email}</p> : <p>Name: {name}</p>}
+      <p>
+        {type.charAt(0).toUpperCase() + type.slice(1)}: {value}
+      </p>
       <Link
-        href={`/profile/edit/${email ? "email" : "name"}`}
+        href={`/profile/edit/${type}`}
         className="btn"
       >
         Edit
