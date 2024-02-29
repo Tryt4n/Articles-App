e432beb04e4fce45e9979b810c43db1b2a44c139
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-02-29 09:07:45 +0100

    Added functionality for password.

--- src/app/profile/components/EditProfileForm.tsx
+++ src/app/profile/components/EditProfileForm.tsx
@@ -8,31 +8,64 @@ type EditProfileForm = { userId: string } & (
   | {
       name: string;
       email?: undefined;
+      password?: undefined;
     }
   | {
       name?: undefined;
       email: string;
+      password?: undefined;
+    }
+  | {
+      name?: undefined;
+      email?: undefined;
+      password: true | null;
     }
 );
 
-export default function EditProfileForm({ userId, name, email }: EditProfileForm) {
-  const isName = typeof name === "string" && typeof email !== "string";
-  const oldValue = isName ? name : email!;
+export default function EditProfileForm({ userId, name, email, password }: EditProfileForm) {
+  let type: "name" | "email" | "password";
+  let oldValue: string;
+  let placeholder: string;
 
-  const formAction = updateUserAction.bind(null, { id: userId, isName, oldValue });
+  if (typeof name === "string") {
+    type = "name";
+    oldValue = name;
+    placeholder = "Enter new name";
+  } else if (typeof email === "string") {
+    type = "email";
+    oldValue = email;
+    placeholder = "Enter new email";
+  } else {
+    type = "password";
+    oldValue = "";
+    placeholder = "Enter new password";
+  }
+
+  const formAction = updateUserAction.bind(null, { id: userId, type, oldValue });
   const [errors, action] = useFormState(formAction, []);
 
   return (
     <form action={action}>
-      <label htmlFor="edit-profile">{isName ? "Name" : "Email"}</label>
+      <label htmlFor="edit-profile">{type.charAt(0).toUpperCase() + type.slice(1)}</label>
       <input
-        type={isName ? "text" : "email"}
+        type={type === "password" ? "password" : type === "email" ? "email" : "text"}
         name="edit-profile"
         id="edit-profile"
-        defaultValue={isName ? name : email}
-        minLength={isName ? 3 : 5}
+        defaultValue={oldValue}
+        minLength={type === "password" ? 8 : 3}
         maxLength={50}
+        placeholder={placeholder}
       />
+      {type === "password" && (
+        <input
+          type="password"
+          name="edit-profile-password-confirmation"
+          id="edit-profile-password-confirmation"
+          placeholder="Confirm password"
+          minLength={8}
+          maxLength={50}
+        />
+      )}
       <button
         type="submit"
         className="btn"
