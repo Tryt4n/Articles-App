499d0625cac7a8821fe739eb19a8427ca5564f49
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-05 12:32:44 +0100

    Deleted logic responsible for displaying posts based on the type passed as prop has been removed.

--- src/app/components/AuthorArticlesList/AuthorArticlesList.tsx
+++ src/app/components/AuthorArticlesList/AuthorArticlesList.tsx
@@ -3,31 +3,14 @@ import { AuthorsArticlesListCard } from "../AuthorArticleCard/AuthorArticleCard"
 import type { Post } from "@/types/posts";
 import "./style.css";
 
-export default function AuthorArticlesList({
-  posts,
-  type = "published",
-}: {
-  posts: Post[];
-  type?: "all" | "published" | "drafts";
-}) {
-  let displayedPosts = posts;
-
-  if (type === "published") {
-    displayedPosts = posts.filter((post) => post.published);
-  }
-
-  if (type === "drafts") {
-    displayedPosts = posts.filter((post) => !post.published);
-  }
-
+export default function AuthorArticlesList({ posts }: { posts: Post[] }) {
   return (
     <ul className="author-articles-list">
-      {displayedPosts.map((post, index) => (
+      {posts.map((post, index) => (
         <AuthorsArticlesListCard
           key={post.id}
           post={post}
           priority={index < 3}
-          type={type}
         />
       ))}
     </ul>
