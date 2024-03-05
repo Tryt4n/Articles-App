8915ab8eb0917510df5da1643d68b64a00ed1c3b
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-05 15:50:55 +0100

    Created new post page.

--- src/app/drafts/new/page.tsx
+++ src/app/drafts/new/page.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import PostForm from "../components/PostForm/PostForm";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 
-export default function NewDraftPage() {
+export default async function NewDraftPage() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
       <h1>New Draft</h1>
 
-      <form action="">
-        <div></div>
-      </form>
+      {session?.user && <PostForm authorId={session.user.id} />}
     </main>
   );
 }
