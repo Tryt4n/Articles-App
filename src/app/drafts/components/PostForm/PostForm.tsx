1d1768fc9d5e03702efdc11f065d9d298a135d7f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-05 15:53:03 +0100

    Adjusted form to be able to use in both creation or edit of post.

--- src/app/drafts/components/PostForm/PostForm.tsx
+++ src/app/drafts/components/PostForm/PostForm.tsx
@@ -2,7 +2,13 @@
 
 import React, { useRef, useState } from "react";
 import { useFormState } from "react-dom";
-import { deletePostAction, editPostAction, publishPostAction } from "@/app/actions/posts";
+import {
+  createAndPublishPostAction,
+  createPostAction,
+  deletePostAction,
+  editPostAction,
+  publishPostAction,
+} from "@/app/actions/posts";
 import { ContentTextArea } from "./components/ContentTextArea";
 import PostTags from "./components/PostTags";
 import SelectedCategoryInput from "./components/SelectedCategoryInput";
@@ -13,42 +19,59 @@ import PublishDraftBtn from "./components/PublishDraftBtn";
 import DeletePostBtn from "./components/DeletePostBtn";
 import type { Post } from "@/types/posts";
 
-export type PostFormProps = {
-  post: Post;
-  postTags: {
-    id: string;
-    name: string;
-  }[];
-};
+export type PostFormProps =
+  | {
+      post: Post;
+      postTags: {
+        id: string;
+        name: string;
+      }[];
+      authorId: string;
+    }
+  | {
+      post?: undefined;
+      postTags?: undefined;
+      authorId: string;
+    };
 
-export default function PostForm({ post, postTags }: PostFormProps) {
-  const [errors, mainAction] = useFormState(editPostAction, {
+export default function PostForm({ post, postTags, authorId }: PostFormProps) {
+  const [errors, mainAction] = useFormState(post ? editPostAction : createPostAction, {
     title: undefined,
     content: undefined,
     tags: undefined,
   });
-  const [textAreaValue, setTextAreaValue] = useState(post.content);
+  const [textAreaValue, setTextAreaValue] = useState(post?.content || "");
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   return (
     <>
       <form action={mainAction}>
-        <input
-          type="hidden"
-          name="post-id"
-          value={post.id}
-        />
+        {post && (
+          <>
+            <input
+              type="hidden"
+              name="post-id"
+              value={post.id}
+            />
+
+            <input
+              type="hidden"
+              name="existing-post-tags"
+              value={`[${postTags.map((tag) => {
+                return JSON.stringify(tag);
+              })}]`}
+            />
+          </>
+        )}
 
         <input
           type="hidden"
-          name="existing-post-tags"
-          value={`[${postTags.map((tag) => {
-            return JSON.stringify(tag);
-          })}]`}
+          name="author-id"
+          value={authorId}
         />
 
         <TitleInput
-          title={post.title}
+          title={post?.title || ""}
           error={errors.title}
         />
 
@@ -64,12 +87,16 @@ export default function PostForm({ post, postTags }: PostFormProps) {
           error={errors.content}
         />
 
-        <SelectedCategoryInput post={post} />
+        <SelectedCategoryInput category={post?.category || "general"} />
 
         <div>
           <SavePostBtn formAction={mainAction} />
-          {!post.published && <PublishDraftBtn formAction={publishPostAction} />}
-          <DeletePostBtn formAction={deletePostAction} />
+
+          {!post?.published && (
+            <PublishDraftBtn formAction={post ? publishPostAction : createAndPublishPostAction} />
+          )}
+
+          {post && <DeletePostBtn formAction={deletePostAction} />}
         </div>
       </form>
 
