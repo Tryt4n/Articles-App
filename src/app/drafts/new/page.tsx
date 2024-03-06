769264a82c862a4400fe4cfe1974690ed07e089f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-06 09:31:54 +0100

    Changed heading and added metadata `title`.

--- src/app/drafts/new/page.tsx
+++ src/app/drafts/new/page.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import PostForm from "../components/PostForm/PostForm";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
+import type { Metadata } from "next/types";
 
-export default async function NewDraftPage() {
+export const metadata: Metadata = {
+  title: "Create New Post",
+};
+
+export default async function NewPostPage() {
   const session = await getServerSession(authOptions);
 
   return (
     <main>
-      <h1>New Draft</h1>
+      <h1>New Post</h1>
 
       {session?.user && <PostForm authorId={session.user.id} />}
     </main>
