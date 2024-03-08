4cb761334f42f1588e230eaa16ff5c3841ee86a9
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-08 08:31:35 +0100

    Added default props.

--- src/app/signup/components/CreateButton.tsx
+++ src/app/signup/components/CreateButton.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { type ComponentPropsWithoutRef } from "react";
 import { useFormStatus } from "react-dom";
 
-export default function CreateButton() {
+export default function CreateButton({ ...props }: ComponentPropsWithoutRef<"button">) {
   const { pending } = useFormStatus();
 
   return (
     <button
+      {...props}
       type="submit"
       className="btn"
       disabled={pending}
