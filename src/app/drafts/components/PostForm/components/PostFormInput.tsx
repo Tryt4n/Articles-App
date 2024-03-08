3652117324f7f7b5e8c7026c86a98cb9096312fe
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-08 13:13:23 +0100

    Created one general input component for post form.

---
+++ src/app/drafts/components/PostForm/components/PostFormInput.tsx
@@ -0,0 +1,39 @@
+import React, { type ComponentProps } from "react";
+import { FormInput } from "@/app/components/FormInput/FormInput";
+
+type PostFormInputProps = ComponentProps<typeof FormInput> & {
+  error?: string;
+};
+
+export const PostFormInput = React.forwardRef<HTMLInputElement, PostFormInputProps>(
+  (props, ref) => {
+    const { label, id, type, defaultValue, error, ...rest } = props;
+
+    return (
+      <FormInput
+        {...rest}
+        label={label}
+        id={`post-${id}`}
+        type={type}
+        required
+        minLength={1}
+        maxLength={100}
+        defaultValue={defaultValue}
+        aria-errormessage={error ? `${id}-error` : undefined}
+        aria-invalid={!!error}
+        ref={ref}
+      >
+        {error && (
+          <strong
+            id={`${id}-error`}
+            aria-live="polite"
+          >
+            {error}
+          </strong>
+        )}
+      </FormInput>
+    );
+  }
+);
+
+PostFormInput.displayName = `PostFormInput}`;
