151b20bcbebaa28b151a20a1792ebea34a9f4de8
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-09 18:42:18 +0100

    Removed unnecessary class.

--- src/app/drafts/(pages)/layout.tsx
+++ src/app/drafts/(pages)/layout.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import "./style.css";
 
 export default function CreatePostLayout({ children }: { children: React.ReactNode }) {
-  return <main className="create-post-container">{children}</main>;
+  return <main>{children}</main>;
 }
