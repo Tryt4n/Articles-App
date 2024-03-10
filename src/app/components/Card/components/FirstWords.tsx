9fc575da5372774e98e0061c283e889cc4c17b5f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-10 14:37:34 +0100

    Added more markdown elements.

--- src/app/components/Card/components/FirstWords.tsx
+++ src/app/components/Card/components/FirstWords.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
 import type { Post } from "@/types/posts";
 
 export default function FirstWords({ content }: { content: Post["content"] }) {
   const specialChars = ["\\*\\*\\*", "\\*\\*", "\\*", "`", "``", "```", "~"]; // Special characters with regex escape
-  let displayContent = content.substring(0, 50).trimEnd(); // Trim the content to 50 characters
+  let displayContent = content.substring(0, 200).trimEnd(); // Trim the content to 2000 characters
 
   // Reverse loop to close text with special characters from the deepest to the shallowest
   for (let i = specialChars.length - 1; i >= 0; i--) {
@@ -18,30 +21,50 @@ export default function FirstWords({ content }: { content: Post["content"] }) {
   }
 
   return (
-    <ReactMarkdown
-      className="post-card-subheader"
-      remarkPlugins={[remarkGfm]}
-      allowedElements={[
-        "p",
-        "em",
-        "del",
-        "strong",
-        "ul",
-        "ol",
-        "li",
-        "img",
-        "code",
-        "pre",
-        "blockquote",
-        "table",
-        "thead",
-        "tbody",
-        "tr",
-        "th",
-        "td",
-      ]}
-    >
-      {displayContent}
-    </ReactMarkdown>
+    <pre className="post-card-subheader">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+        components={{
+          // Override the default 'a' component
+          a: ({ node, ...props }) => (
+            <span
+              className="post-card-subheader-link"
+              {...props}
+            >
+              link
+            </span>
+          ),
+        }}
+        allowedElements={[
+          "h2",
+          "h3",
+          "h4",
+          "h5",
+          "h6",
+          "a", // "a" is replaced with "span"
+          "p",
+          "em",
+          "del",
+          "strong",
+          "ul",
+          "ol",
+          "li",
+          "code",
+          "pre",
+          "blockquote",
+          "table",
+          "thead",
+          "tbody",
+          "tr",
+          "th",
+          "td",
+          "span",
+          "input",
+        ]}
+      >
+        {displayContent}
+      </ReactMarkdown>
+    </pre>
   );
 }
