02429c7621d1bf588e187ed74645bbd075752bc1
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-10 09:13:13 +0100

    Created new date object only when it's not already date.

--- src/app/components/Card/components/Time.tsx
+++ src/app/components/Card/components/Time.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { format } from "date-fns/format";
 
 export default function Time({ time }: { time: string | Date }) {
-  const postPublishedDate = new Date(time);
+  const postPublishedDate = time instanceof Date ? time : new Date(time);
 
   return (
     <time
