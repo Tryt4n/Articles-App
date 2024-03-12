cb0c8157da70371daa6c4e5acf223fd0384f2214
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-12 16:34:22 +0100

    Added custom function to use `startViewTransition` API.

--- src/app/helpers/helpers.ts
+++ src/app/helpers/helpers.ts
@@ -1,3 +1,18 @@
+export function startViewTransition(callback: () => void) {
+  // Check if the user's system prefers reduced motion
+  const prefersMotion = window.matchMedia("(prefers-reduced-motion: reduce)");
+
+  // If the startViewTransition method doesn't exist on the document object or the user's system prefers reduced motion, call the callback function
+  if (!document.startViewTransition || prefersMotion.matches) {
+    callback();
+  } else {
+    // Otherwise, start the view transition and call the callback function
+    document.startViewTransition(() => {
+      callback();
+    });
+  }
+}
+
 export function wait(duration: number) {
   return new Promise((resolve) => setTimeout(resolve, duration));
 }
