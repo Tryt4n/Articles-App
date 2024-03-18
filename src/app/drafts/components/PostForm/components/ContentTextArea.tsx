3c2ef252f76ec4896d37460666b9c743abcd6f09
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-18 16:35:47 +0100

    Moved inputs logic into its own components.

--- src/app/drafts/components/PostForm/components/ContentTextArea.tsx
+++ src/app/drafts/components/PostForm/components/ContentTextArea.tsx
@@ -1,49 +1,46 @@
-import React, { forwardRef, type ForwardedRef } from "react";
-import useModal from "@/app/hooks/useModal";
-import ModalMarkdownInfo from "./ModalMarkdownInfo/ModalMarkdownInfo";
+import React, { useState, useEffect, useDeferredValue, forwardRef, type ForwardedRef } from "react";
+import usePost from "@/app/drafts/(pages)/hooks/usePost";
 import TextareaAutosize, { type TextareaAutosizeProps } from "react-textarea-autosize";
-import InfoIcon from "@/app/Icons/InfoIcon";
 
 export const ContentTextArea = forwardRef(InnerComponent);
 
-type ContentTextAreaProps = {
-  error?: string;
-} & TextareaAutosizeProps;
+type ContentTextAreaProps = { content: string; error?: string } & TextareaAutosizeProps;
 
-function InnerComponent(
-  { error, ...props }: ContentTextAreaProps,
+export default function InnerComponent(
+  { content, error, ...props }: ContentTextAreaProps,
   ref: ForwardedRef<HTMLTextAreaElement>
 ) {
-  const { showModal } = useModal();
+  const { postData, setPostData } = usePost();
+  const [textareaContent, setTextareaContent] = useState(content);
+  const deferredContent = useDeferredValue(textareaContent);
+
+  // Update global state with deferred value to prevent lag
+  useEffect(() => {
+    setPostData((prevPostData) => ({ ...prevPostData, content: deferredContent }));
+  }, [deferredContent, setPostData]);
+
+  // Update local storage for post preview page
+  useEffect(() => {
+    localStorage.setItem(
+      "live-preview-data",
+      JSON.stringify({ ...postData, content: textareaContent })
+    );
+  }, [textareaContent, postData]);
 
   return (
-    <fieldset>
-      <div className="post-content-form-inner-wrapper">
-        <legend>Fully supported markdown</legend>
-        <button
-          type="button"
-          className="post-content-info-btn"
-          aria-controls="modal"
-          aria-haspopup="dialog"
-          onClick={showModal}
-        >
-          How it works?
-          <InfoIcon />
-        </button>
-      </div>
-
+    <>
       <label htmlFor="post-content">Content:</label>
       <TextareaAutosize
-        required
-        minLength={100}
-        placeholder="Write your post here..."
         {...props}
-        id="post-content"
-        name="post-content"
         ref={ref}
+        defaultValue={textareaContent}
+        placeholder="Write your post here..."
+        minLength={100}
+        required
         aria-errormessage={error ? "content-error" : undefined}
         aria-invalid={!!error}
         aria-describedby="modal"
+        onChange={(e) => setTextareaContent(e.target.value)}
       />
       {error && (
         <strong
@@ -53,8 +50,6 @@ function InnerComponent(
           {error}
         </strong>
       )}
-
-      <ModalMarkdownInfo id="modal" />
-    </fieldset>
+    </>
   );
 }
