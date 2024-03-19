be84b1f1f6c48d5da91bec0a26ca3d77651625e2
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-19 10:09:43 +0100

    Created refs for post form components.

--- src/app/drafts/context/PostContext.tsx
+++ src/app/drafts/context/PostContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { createContext, useState } from "react";
+import React, { createContext, useRef, useState } from "react";
 import type { PostProps } from "@/app/components/Post/Post";
 
 type PostContextType = {
@@ -8,6 +8,15 @@ type PostContextType = {
   setPostData: React.Dispatch<React.SetStateAction<PostProps>>;
   textOptions: TextOptions;
   setTextOptions: React.Dispatch<React.SetStateAction<TextOptions>>;
+  refs: {
+    titleRef: React.RefObject<HTMLInputElement>;
+    imageRef: React.RefObject<HTMLInputElement>;
+    tagsRef: React.RefObject<HTMLInputElement>;
+    selectedCategoryRef: React.RefObject<HTMLSelectElement>;
+    textareaRef: React.RefObject<HTMLTextAreaElement>;
+    colorPickerRef: React.RefObject<HTMLInputElement>;
+    fontSizeInputRef: React.RefObject<HTMLInputElement>;
+  };
 };
 
 type TextOptions = {
@@ -34,11 +43,28 @@ export default function PostContextProvider({ children }: { children: React.Reac
     cursorPosition: 0,
   });
 
+  const titleRef = useRef<HTMLInputElement>(null);
+  const imageRef = useRef<HTMLInputElement>(null);
+  const tagsRef = useRef<HTMLInputElement>(null);
+  const selectedCategoryRef = useRef<HTMLSelectElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const colorPickerRef = useRef<HTMLInputElement>(null);
+  const fontSizeInputRef = useRef<HTMLInputElement>(null);
+
   const contextValues = {
     postData,
     setPostData,
     textOptions,
     setTextOptions,
+    refs: {
+      titleRef,
+      imageRef,
+      tagsRef,
+      selectedCategoryRef,
+      textareaRef,
+      colorPickerRef,
+      fontSizeInputRef,
+    },
   };
 
   return <PostContext.Provider value={contextValues}>{children}</PostContext.Provider>;
