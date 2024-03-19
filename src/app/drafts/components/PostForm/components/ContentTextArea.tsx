49617fabc806c65dc49f06dc8cc2886ea7d5d7aa
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-19 10:12:16 +0100

    Using `ref` from context instead of `forwardRef`. Moved `onSelect` and `onClick` function from `<PostForm/>`.

--- src/app/drafts/components/PostForm/components/ContentTextArea.tsx
+++ src/app/drafts/components/PostForm/components/ContentTextArea.tsx
@@ -1,18 +1,42 @@
-import React, { useState, useEffect, useDeferredValue, forwardRef, type ForwardedRef } from "react";
-import usePost from "@/app/drafts/(pages)/hooks/usePost";
+import React, { useState, useEffect, useDeferredValue, useMemo } from "react";
+import usePostForm from "@/app/drafts/(pages)/hooks/usePostForm";
 import TextareaAutosize, { type TextareaAutosizeProps } from "react-textarea-autosize";
-
-export const ContentTextArea = forwardRef(InnerComponent);
 
 type ContentTextAreaProps = { content: string; error?: string } & TextareaAutosizeProps;
 
-export default function InnerComponent(
-  { content, error, ...props }: ContentTextAreaProps,
-  ref: ForwardedRef<HTMLTextAreaElement>
-) {
-  const { postData, setPostData } = usePost();
+export default function ContentTextArea({ content, error, ...props }: ContentTextAreaProps) {
+  const { postData, setPostData, textOptions, setTextOptions, refs } = usePostForm();
+
   const [textareaContent, setTextareaContent] = useState(content);
+  const [selectedText, setSelectedText] = useState(textOptions.selectedText);
+  const [cursorPosition, setCursorPosition] = useState(textOptions.cursorPosition);
+
   const deferredContent = useDeferredValue(textareaContent);
+  const deferredSelectedText = useDeferredValue(selectedText);
+  const deferredCursorPosition = useDeferredValue(cursorPosition);
+
+  function handleOnChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setTextareaContent(e.target.value);
+  }
+
+  function handleOnSelect(e: React.SyntheticEvent<HTMLTextAreaElement, Event>) {
+    const target = e.target as HTMLTextAreaElement;
+    setSelectedText(target.value.substring(target.selectionStart, target.selectionEnd));
+  }
+
+  function handleOnClick(e: React.MouseEvent<HTMLTextAreaElement, MouseEvent>) {
+    const target = e.target as HTMLTextAreaElement;
+    setCursorPosition(target.selectionStart);
+  }
+
+  // Update text options with deferred value to prevent lag
+  useEffect(() => {
+    setTextOptions((prevTextOptions) => ({
+      ...prevTextOptions,
+      selectedText: deferredSelectedText,
+      cursorPosition: deferredCursorPosition,
+    }));
+  }, [deferredCursorPosition, deferredSelectedText, setTextOptions]);
 
   // Update global state with deferred value to prevent lag
   useEffect(() => {
@@ -20,19 +44,22 @@ export default function InnerComponent(
   }, [deferredContent, setPostData]);
 
   // Update local storage for post preview page
+  const livePreviewData = useMemo(
+    () => JSON.stringify({ ...postData, content: textareaContent }),
+    [textareaContent, postData]
+  );
+
   useEffect(() => {
-    localStorage.setItem(
-      "live-preview-data",
-      JSON.stringify({ ...postData, content: textareaContent })
-    );
-  }, [textareaContent, postData]);
+    localStorage.setItem("live-preview-data", livePreviewData);
+  }, [livePreviewData]);
 
   return (
     <>
       <label htmlFor="post-content">Content:</label>
       <TextareaAutosize
         {...props}
-        ref={ref}
+        className="post-content-form-textarea"
+        ref={refs.textareaRef}
         defaultValue={textareaContent}
         placeholder="Write your post here..."
         minLength={100}
@@ -40,7 +67,9 @@ export default function InnerComponent(
         aria-errormessage={error ? "content-error" : undefined}
         aria-invalid={!!error}
         aria-describedby="modal"
-        onChange={(e) => setTextareaContent(e.target.value)}
+        onChange={handleOnChange}
+        onSelect={handleOnSelect}
+        onClick={handleOnClick}
       />
       {error && (
         <strong
