94c948323180d6fe72c390be3ab07347d16a7805
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-19 10:11:14 +0100

    Using `ref` from context instead of `forwardRef` and moved function `handleChangeText` from `<PostForm/>`.

--- src/app/drafts/components/PostForm/components/ChangeTextBtn.tsx
+++ src/app/drafts/components/PostForm/components/ChangeTextBtn.tsx
@@ -1,12 +1,56 @@
 import React, { type ComponentPropsWithoutRef } from "react";
-import usePost from "@/app/drafts/(pages)/hooks/usePost";
+import usePostForm from "@/app/drafts/(pages)/hooks/usePostForm";
 
-type ChangeTextBtnProps = {
-  handleChangeText: () => void;
-} & ComponentPropsWithoutRef<"button">;
+export default function ChangeTextBtn({ ...props }: ComponentPropsWithoutRef<"button">) {
+  const { refs, textOptions, setTextOptions, setPostData } = usePostForm();
 
-export default function ChangeTextBtn({ handleChangeText, ...props }: ChangeTextBtnProps) {
-  const { textOptions } = usePost();
+  function handleChangeText() {
+    const defaultColor = "#000000";
+    const defaultFontSize = "16px";
+
+    const textArea = refs.textareaRef.current;
+    const { textColor, textSize, cursorPosition, selectedText } = textOptions;
+
+    if (!textArea) return;
+
+    // Example `insertText` value: [selectedText[color: #000000, size: 16px]]
+    const insertText = `[${selectedText}[color: ${
+      textColor !== defaultColor ? textColor : defaultColor
+    }, size: ${textSize !== defaultFontSize ? textSize : defaultFontSize}]]`;
+
+    let newCursorPosition = cursorPosition;
+
+    // If there is a selected text and the user changed the color or font size
+    if (selectedText !== "" && (textColor !== defaultColor || textSize !== defaultFontSize)) {
+      const start = textArea.selectionStart; // Start of the selected text
+      const end = textArea.selectionEnd; // End of the selected text
+      textArea.value =
+        textArea.value.substring(0, start) + insertText + textArea.value.substring(end); // Replace the selected text with the new one
+      setTextOptions((prevValue) => ({ ...prevValue, selectedText: insertText }));
+
+      newCursorPosition = start + insertText.length;
+    }
+
+    if (selectedText === "" && (textColor !== defaultColor || textSize !== defaultFontSize)) {
+      const updatedContent = `${textArea.value.slice(
+        0,
+        cursorPosition
+      )} ${insertText} ${textArea.value.slice(cursorPosition)}`;
+
+      textArea.value = updatedContent;
+
+      // Focus will be directly where the user should start typing
+      newCursorPosition = cursorPosition + insertText.indexOf("[") + 2;
+    }
+
+    // Update global state
+    setPostData((prevValue) => ({ ...prevValue, content: textArea.value }));
+
+    // Set focus on the textarea at the end of the inserted text
+    textArea.focus();
+    textArea.selectionStart = newCursorPosition;
+    textArea.selectionEnd = newCursorPosition;
+  }
 
   return (
     <button
