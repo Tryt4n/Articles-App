cfae1e7d17ab700cd8343faa6d6ee04bf1c70ced
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-21 18:50:17 +0100

    Changed comment type.

--- src/types/comments.ts
+++ src/types/comments.ts
@@ -1,3 +1,9 @@
 import type { Comment as dbComment } from "@prisma/client";
+import type { User } from "./users";
 
-export type Comment = dbComment;
+export type Comment = dbComment & {
+  author: {
+    name: User["name"];
+    image: User["image"];
+  };
+};
