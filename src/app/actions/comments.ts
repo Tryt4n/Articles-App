e68588910c4cf9d0188cd9cc38ce167ff3020c71
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-23 16:37:20 +0100

    Changed types.

--- src/app/actions/comments.ts
+++ src/app/actions/comments.ts
@@ -2,12 +2,15 @@
 
 import { createComment } from "@/db/comments";
 import { revalidatePath } from "next/cache";
+import type { Comment } from "@/types/comments";
+import type { User } from "@/types/users";
+import type { Post } from "@/types/posts";
 
 export async function postComment(formData: FormData) {
-  const comment = formData.get("new-comment") as string;
-  const postId = formData.get("post-id") as string;
-  const authorId = formData.get("author-id") as string;
-  const replyToId = formData.get("reply-to") as string | undefined;
+  const comment = formData.get("new-comment") as Comment["content"];
+  const postId = formData.get("post-id") as Post["id"];
+  const authorId = formData.get("author-id") as User["id"];
+  const replyToId = formData.get("reply-to") as Comment["id"] | undefined;
 
   const newComment = {
     content: comment,
