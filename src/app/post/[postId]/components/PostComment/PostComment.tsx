c7cd81ed7ac86a8bbdcef81d17fed03f1abd6b9e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-24 16:12:29 +0100

    Included displaying replies in comment.

--- src/app/post/[postId]/components/PostComment/PostComment.tsx
+++ src/app/post/[postId]/components/PostComment/PostComment.tsx
@@ -4,9 +4,30 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import Image from "next/image";
 import Time from "../../../../components/Time/Time";
 import ReplyBtn from "@/app/post/[postId]/components/ReplyBtn";
+import DeleteCommentBtn from "../DeleteCommentBtn/DeleteCommentBtn";
 import type { Comment } from "@/types/comments";
 
-export default async function PostComment({ comment }: { comment: Comment }) {
+export default function PostComment({ comment }: { comment: Comment }) {
+  return (
+    <Comment comment={comment}>
+      {comment.replies && comment.replies.length > 0 && (
+        <ul style={{ border: "3px solid #666", margin: "1em" }}>
+          {comment.replies.map((reply) => (
+            <li
+              key={reply.id}
+              style={{ borderBottom: "1px solid black" }}
+            >
+              <p>{reply.content}</p>
+              <strong>{reply.author.name}</strong>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Comment>
+  );
+}
+
+async function Comment({ comment, children }: { comment: Comment; children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
 
   const user = comment.author.name === session?.user?.name ? "You" : comment.author.name;
@@ -15,7 +36,10 @@ export default async function PostComment({ comment }: { comment: Comment }) {
     comment.author.name === session?.user?.name ? "Your avatar." : `${user}'s avatar.`;
 
   return (
-    <li key={comment.id}>
+    <li
+      key={comment.id}
+      style={{ backgroundColor: "#99999980", marginBlock: "2em" }}
+    >
       <div>
         <div>
           <Image
@@ -47,7 +71,16 @@ export default async function PostComment({ comment }: { comment: Comment }) {
         />
       )}
 
+      {session && comment.author.name === session.user.name && (
+        <DeleteCommentBtn
+          postId={comment.postId}
+          commentId={comment.id}
+        />
+      )}
+
       <p>{comment.content}</p>
+
+      {children}
     </li>
   );
 }
