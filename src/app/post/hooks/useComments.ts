d4d9c47ba21d81fee573cc94f81914ac9defb8d7
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-29 13:41:41 +0100

    Changed directory path.

--- src/app/post/hooks/useComments.ts
+++ src/app/post/hooks/useComments.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { CommentsContext } from "../context/CommentsContext";
+import { CommentsContext } from "../[postId]/context/CommentsContext";
 
 export default function useComments() {
   const post = useContext(CommentsContext);
