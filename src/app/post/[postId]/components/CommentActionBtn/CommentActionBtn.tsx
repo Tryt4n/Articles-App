0617844af3f4f249620056f30137c5f3ec3ce772
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-29 13:45:48 +0100

    Created button component for action in comment form.

---
+++ src/app/post/[postId]/components/CommentActionBtn/CommentActionBtn.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { type ComponentPropsWithoutRef } from "react";
+import useComments from "@/app/post/hooks/useComments";
+import { startViewTransition } from "@/app/helpers/helpers";
+import type { Comment } from "@/types/comments";
+
+type CommentActionBtnProps = {
+  status: "edit" | "reply";
+  comment: Comment;
+} & ComponentPropsWithoutRef<"button">;
+
+export default function CommentActionBtn({ status, comment, ...props }: CommentActionBtnProps) {
+  const { formCommentStatus, setFormCommentStatus, currentComment, setCurrentComment } =
+    useComments();
+
+  function openCommentForm() {
+    startViewTransition(() => {
+      setFormCommentStatus(status);
+      setCurrentComment(comment);
+    });
+  }
+
+  return (
+    <>
+      <button
+        {...props}
+        type="button"
+        onClick={openCommentForm}
+        disabled={formCommentStatus === status && currentComment?.id === comment.id}
+      >
+        {`${status.charAt(0).toUpperCase() + status.slice(1)}${
+          formCommentStatus === status && currentComment?.id === comment.id ? "ing" : ""
+        }`}
+      </button>
+    </>
+  );
+}
