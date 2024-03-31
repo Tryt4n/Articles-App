2cf914a9d0711aa8367507a399538e98820769ac
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-03-31 11:23:14 +0200

    Created actions for liking.

---
+++ src/app/actions/likes.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { likeComment, likePost } from "@/db/likes";
+import { revalidatePath } from "next/cache";
+import type { Comment } from "@/types/comments";
+import type { Post } from "@/types/posts";
+import type { User } from "@/types/users";
+
+export async function likeAction(formData: FormData) {
+  const commentId = formData.get("comment-id") as Comment["id"] | undefined;
+  const postId = formData.get("post-id") as Post["id"];
+  const userId = formData.get("user-id") as User["id"];
+
+  commentId ? likeComment(userId, commentId) : likePost(userId, postId);
+  revalidatePath(`/post/${postId}`);
+}
