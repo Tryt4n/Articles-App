32306143602dc72ae7b2b59427652508370f77e8
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-02 16:25:04 +0200

    Created save post button.

---
+++ src/app/components/SavePostBtn/SavePostBtn.tsx
@@ -0,0 +1,41 @@
+import React, { type ComponentPropsWithoutRef } from "react";
+import { savePostAction } from "@/app/actions/posts";
+import SaveBtn from "./SaveBtn";
+import type { Post } from "@/types/posts";
+import type { User } from "@/types/users";
+import "./style.css";
+
+type SavePostBtnProps = {
+  postId: Post["id"];
+  authorId: User["id"];
+  alreadySaved: boolean;
+} & ComponentPropsWithoutRef<"button">;
+
+export default function SavePostBtn({
+  postId,
+  authorId,
+  alreadySaved = false,
+  ...props
+}: SavePostBtnProps) {
+  return (
+    <form action={savePostAction}>
+      <input
+        type="hidden"
+        name="post-id"
+        value={postId}
+      />
+      <input
+        type="hidden"
+        name="user-id"
+        value={authorId}
+      />
+
+      <SaveBtn
+        {...props}
+        alreadySaved={alreadySaved}
+        className={`save-post-btn${props.className ? ` ${props.className}` : ""}`}
+        aria-label={`Click to ${alreadySaved ? "un" : ""}save this post in your saved posts list.`}
+      />
+    </form>
+  );
+}
