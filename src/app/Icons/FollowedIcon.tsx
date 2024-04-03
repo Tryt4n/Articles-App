53c650d932579ee04c37de83bf737756e18e78e6
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-03 09:33:28 +0200

    Created icon.

---
+++ src/app/Icons/FollowedIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function FollowedIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      aria-hidden="true"
+    >
+      <g fill="#101820">
+        <path
+          d="M20 30a8 8 0 1 1 8-8 8 8 0 0 1-8 8Zm0-14a6 6 0 1 0 6 6 6 6 0 0 0-6-6Z"
+          fill="#000000"
+        ></path>
+        <path
+          d="M19 25a1 1 0 0 1-.71-.29l-2-2a1 1 0 0 1 1.41-1.41l1.3 1.29 3.29-3.29a1 1 0 0 1 1.41 1.41l-4 4a1 1 0 0 1-.7.29zm-6-13a5 5 0 1 1 5-5 5 5 0 0 1-5 5zm0-8a3 3 0 1 0 3 3 3 3 0 0 0-3-3zm-2 23H5a1 1 0 0 1-1-1v-4a9 9 0 0 1 9-9 1 1 0 0 1 0 2 7 7 0 0 0-7 7v3h5a1 1 0 0 1 0 2z"
+          fill="#000000"
+        ></path>
+      </g>
+    </svg>
+  );
+}
---
+++ src/app/Icons/HeartFilledIcon.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HeartFilledIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      viewBox="0 0 29 29"
+      aria-hidden="true"
+    >
+      <path d="m14.854 6.083-.354.353-.354-.354a6.5 6.5 0 0 0-9.192 9.192l.354.354L14.5 24.82l9.192-9.192.354-.354a6.5 6.5 0 0 0-9.192-9.191z" />
+      <path d="m14.854 6.083-.354.353-.354-.354a6.5 6.5 0 0 0-9.192 9.192l.354.354L14.5 24.82l9.192-9.192.354-.354a6.5 6.5 0 0 0-9.192-9.191z" />
+    </svg>
+  );
+}
