4f0a978dfc1576bfdc4af67098aa4a8c9ecd5ef0
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-04-10 08:52:14 +0200

    Added env file with zod validation and autocomplete.

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().min(1),
+  NEXTAUTH_SECRET: z.string().min(1),
+  GITHUB_SECRET: z.string().min(1),
+  GITHUB_ID: z.string().min(1),
+});
+
+export const parsedEnv = envSchema.parse(process.env);
